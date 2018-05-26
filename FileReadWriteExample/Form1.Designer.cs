cb9cb80cef8ad4e24e0d376078d795b56d472298
RajaVamsi Chidella <rajavamsi.chidella@gmail.com>
2018-05-26 15:39:14 -0500

    FileReadWriteUsingClass

---
+++ FileReadWriteExample/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FileReadWriteExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxWrite = new System.Windows.Forms.TextBox();
+            this.btnWriteRead = new System.Windows.Forms.Button();
+            this.lblReadFile = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxWrite
+            // 
+            this.txtBoxWrite.Location = new System.Drawing.Point(260, 59);
+            this.txtBoxWrite.Name = "txtBoxWrite";
+            this.txtBoxWrite.Size = new System.Drawing.Size(145, 22);
+            this.txtBoxWrite.TabIndex = 0;
+            this.txtBoxWrite.Text = "Write this text into a File";
+            // 
+            // btnWriteRead
+            // 
+            this.btnWriteRead.Location = new System.Drawing.Point(271, 132);
+            this.btnWriteRead.Name = "btnWriteRead";
+            this.btnWriteRead.Size = new System.Drawing.Size(121, 43);
+            this.btnWriteRead.TabIndex = 1;
+            this.btnWriteRead.Text = "Write To and Read From File";
+            this.btnWriteRead.UseVisualStyleBackColor = true;
+            this.btnWriteRead.Click += new System.EventHandler(this.btnWriteRead_Click);
+            // 
+            // lblReadFile
+            // 
+            this.lblReadFile.AutoSize = true;
+            this.lblReadFile.Location = new System.Drawing.Point(288, 229);
+            this.lblReadFile.Name = "lblReadFile";
+            this.lblReadFile.Size = new System.Drawing.Size(64, 17);
+            this.lblReadFile.TabIndex = 2;
+            this.lblReadFile.Text = "ReadFile";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(277, 298);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(115, 46);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete Logs";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lblReadFile);
+            this.Controls.Add(this.btnWriteRead);
+            this.Controls.Add(this.txtBoxWrite);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxWrite;
+        private System.Windows.Forms.Button btnWriteRead;
+        private System.Windows.Forms.Label lblReadFile;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ FileReadWriteExample/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileReadWriteExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWriteRead_Click(object sender, EventArgs e)
+        {
+            //StreamWriter writeToFile = File.CreateText("C:/Users/pavan/Desktop/NewRandom/WriteAndReadFile.txt");
+            //string a = txtBoxWrite.Text;
+            //writeToFile.WriteLine(a);
+            //writeToFile.Close();
+
+            //writeFile(txtBoxWrite.Text, "C:/Users/pavan/Desktop/NewRandom/WriteAndReadFile.txt");
+
+            //lblReadFile.Text = readFile("C:/Users/pavan/Desktop/NewRandom/WriteAndReadFile.txt");
+
+            FileWrite fWrite = new FileWrite();
+            fWrite.fileContent = txtBoxWrite.Text;
+            fWrite.filePath = "C:/Users/pavan/Desktop/NewRandom/WriteAndReadFile.txt";
+            // fWrite.writeFile(txtBoxWrite.Text, "C:/Users/pavan/Desktop/NewRandom/WriteAndReadFile.txt");
+            fWrite.writeFile();
+            FileRead fRead = new FileRead();
+            lblReadFile.Text = fRead.readFile("C:/Users/pavan/Desktop/NewRandom/WriteAndReadFile.txt");
+            
+        }
+        //public void writeFile(string fileContent, string filePath)
+        //{
+        //    StreamWriter writeToFile = File.CreateText(filePath);
+        //    string a = fileContent;
+        //    writeToFile.WriteLine(a);
+        //    writeToFile.Close();
+        //}
+        //public string readFile(string filePath)
+        //{
+        //    StreamReader ReadFromFile = new StreamReader(filePath);
+        //    string b = ReadFromFile.ReadLine();
+        //    return b;
+
+        //}
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            FileDeleteLogs fDelete = new FileDeleteLogs();
+            fDelete.filePath = @"C:/Users/pavan/Desktop/NewRandom/WriteAndReadFile.txt";
+            fDelete.deleteLogs();
+        }
+    }
+    public class FileWrite
+    {
+        public string filePath;
+        public string fileContent;
+        public void writeFile()
+        {
+            StreamWriter writeToFile = File.AppendText(filePath);
+            string a = fileContent;
+            writeToFile.WriteLine(a);
+            writeToFile.Close();
+        }
+    }
+    public class FileRead
+    {
+        string filePath;
+        public string readFile(string filePath)
+        {
+            StreamReader ReadFromFile = new StreamReader(filePath);
+            string b = ReadFromFile.ReadLine();
+            ReadFromFile.Close();
+            return b;
+
+        }
+    }
+
+    public class FileDeleteLogs
+    {
+        public string filePath;
+        public void deleteLogs()
+        {
+            File.Delete(filePath);
+        }
+    }
+}
+        
+
+        
+
